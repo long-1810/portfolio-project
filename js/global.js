0915cc88506c8a0e19422d026252020d215332fa
CopperII <minhlong18102007@gmail.com>
2026-06-07 17:30:34 +0700

    Fix 1 - Attempt 1 fixing links

--- js/global.js
+++ js/global.js
@@ -1,10 +1,17 @@
+const BASE =
+  window.location.hostname === "localhost"
+    ? "/"
+    : "/portfolio-project/";
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggleBtn = document.getElementById("theme-toggle-btn");
     const themeIconImg = document.getElementById("theme-icon-img");
     const body = document.body;
 
-    const iconDarkTheme = "/assets/logo/moon.svg";
-    const iconLightTheme = "/assets/logo/sun.svg";
+    const iconDarkTheme = `${BASE}/assets/logo/moon.svg`;
+    const iconLightTheme = `${BASE}/assets/logo/sun.svg`;
 
     const savedTheme = localStorage.getItem("theme");
     if (savedTheme === "light") {
