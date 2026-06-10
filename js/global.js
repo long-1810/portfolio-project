c35f666ef1c74becccee1c7b82d21fbbd173bc3a
CopperII <minhlong18102007@gmail.com>
2026-06-10 16:38:08 +0700

    Test 1 - Fix paths and links

--- js/global.js
+++ js/global.js
@@ -1,35 +1,34 @@
-// const BASE =
-//   window.location.hostname === "localhost"
-//     ? "/"
-//     : "/portfolio-project/";
+const isGitHubPages = location.hostname.includes("github.io");
 
+const BASE = isGitHubPages
+  ? "/portfolio-project/"
+  : "/";
 
+  document.addEventListener("DOMContentLoaded", () => {
+    const themeToggleBtn = document.getElementById("theme-toggle-btn");
+    const themeIconImg = document.getElementById("theme-icon-img");
+    const body = document.body;
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const themeToggleBtn = document.getElementById("theme-toggle-btn");
-//     const themeIconImg = document.getElementById("theme-icon-img");
-//     const body = document.body;
+    const iconDarkTheme = `${BASE}assets/logo/moon.svg`;
+    const iconLightTheme = `${BASE}assets/logo/sun.svg`;
 
-//     const iconDarkTheme = `${BASE}assets/logo/moon.svg`;
-//     const iconLightTheme = `${BASE}assets/logo/sun.svg`;
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "light") {
+        body.classList.add("light-mode");
+        themeIconImg.src = iconLightTheme;
+    } else {
+        themeIconImg.src = iconDarkTheme;
+    }
 
-//     const savedTheme = localStorage.getItem("theme");
-//     if (savedTheme === "light") {
-//         body.classList.add("light-mode");
-//         themeIconImg.src = iconLightTheme;
-//     } else {
-//         themeIconImg.src = iconDarkTheme;
-//     }
-
-//     themeToggleBtn.addEventListener("click", () => {
-//         body.classList.toggle("light-mode");
+    themeToggleBtn.addEventListener("click", () => {
+        body.classList.toggle("light-mode");
         
-//         if (body.classList.contains("light-mode")) {
-//             themeIconImg.src = iconLightTheme;
-//             localStorage.setItem("theme", "light");
-//         } else {
-//             themeIconImg.src = iconDarkTheme;
-//             localStorage.setItem("theme", "dark");
-//         }
-//     });
-// });
+        if (body.classList.contains("light-mode")) {
+            themeIconImg.src = iconLightTheme;
+            localStorage.setItem("theme", "light");
+        } else {
+            themeIconImg.src = iconDarkTheme;
+            localStorage.setItem("theme", "dark");
+        }
+    });
+});
